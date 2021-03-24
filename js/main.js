4a78bcb7a630fc9617338b0ee15d6fa2798d6471
aa-ag <aaron.aguerrevere@gmail.com>
2021-03-23 19:49:47 -0500

    decided to try a nav bar, that way the page won't have too much scrolling; instead, I might try with modals and links

--- js/main.js
+++
@@ -1,8 +0,0 @@
-GitHubCalendar(".calendar", "aa-ag", { responsive: true });
-
-// Use a proxy
-GitHubCalendar(".calendar", "aa-ag", {
-    proxy(username) {
-        return fetch(`https://your-proxy.com/github?user=${username}`)
-    }
-}).then(r => r.text())
