0d8c5205ea902aed9f981e5dbff053084fd0d3d2
aa-ag <aaron.aguerrevere@gmail.com>
2022-03-05 21:12:47 -0600

    Insert CUSTOM ELEMENTS section to start trying to use custom elements instead of spams in index

--- js/main.js
+++ js/main.js
@@ -76,3 +76,14 @@ $(document).ready(function () {
     $(".footer").delay(3000).fadeIn('slow');
 });
 
+
+//--------------- CUSTOM ELEMENTS ---------------
+class Salutation extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `<h1>Hello World...</h1>`;
+      this.style.color = "grey";
+    }
+  }
+  
+customElements.define("salutation-element", Salutation);
+  
